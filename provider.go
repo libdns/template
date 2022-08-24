483d66f17359039d83073e87d27055c997b022c3
Pablo Lorenzo <plorenzo@hey.com>
2022-08-24 16:02:21 +0800

    Rename module and update dependecy version

--- provider.go
+++ provider.go
@@ -1,7 +1,6 @@
-// Package libdnstemplate implements a DNS record management client compatible
-// with the libdns interfaces for <PROVIDER NAME>. TODO: This package is a
-// template only. Customize all godocs for actual implementation.
-package libdnstemplate
+// Package dinahosting implements a DNS record management client compatible
+// with the libdns interfaces for Dinahosting (https://es.dinahosting.com/api).
+package dinahosting
 
 import (
 	"context"
