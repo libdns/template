5b7029990156afe4b5cf96be9b60e3db2c977a0c
Pablo Lorenzo <plorenzo@hey.com>
2022-08-25 13:51:26 +0800

    Add GetRecords() implementation

--- provider.go
+++ provider.go
@@ -4,27 +4,98 @@ package dinahosting
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
 
 	"github.com/libdns/libdns"
 )
 
-// TODO: Providers must not require additional provisioning steps by the callers; it
-// should work simply by populating a struct and calling methods on it. If your DNS
-// service requires long-lived state or some extra provisioning step, do it implicitly
-// when methods are called; sync.Once can help with this, and/or you can use a
-// sync.(RW)Mutex in your Provider struct to synchronize implicit provisioning.
+const (
+	endpointBase = "https://dinahosting.com/special/api.php"
+)
 
-// Provider facilitates DNS record manipulation with <TODO: PROVIDER NAME>.
+// Provider facilitates DNS record manipulation with Dinahosting.
 type Provider struct {
-	// TODO: put config fields here (with snake_case json
-	// struct tags on exported fields), for example:
-	APIToken string `json:"api_token,omitempty"`
+	Username string `json:"username,omitempty"`
+	Password string `json:"password,omitempty"`
 }
 
 // GetRecords lists all the records in the zone.
+//
+// API docs: https://es.dinahosting.com/api/documentation
+//
+// Full endpoint; https://dinahosting.com/special/api.php?AUTH_USER=user&AUTH_PWD=pass&domain=example.com&responseType=json&command=Domain_Zone_GetAll
 func (p *Provider) GetRecords(ctx context.Context, zone string) ([]libdns.Record, error) {
-	return nil, fmt.Errorf("TODO: not implemented")
+	endpoint, err := url.Parse(endpointBase)
+	if err != nil {
+		return nil, err
+	}
+
+	params := url.Values{}
+	params.Add("AUTH_USER", p.Username)
+	params.Add("AUTH_PWD", p.Password)
+	params.Add("domain", strings.TrimSuffix(zone, "."))
+	params.Add("responseType", "json")
+	params.Add("command", "Domain_Zone_GetAll")
+	endpoint.RawQuery = params.Encode()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, endpoint.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	client := &http.Client{Timeout: 10 * time.Second}
+
+	var response struct {
+		TrID         string `json:"trId,omitempty"`
+		ResponseCode int16  `json:"responseCode,omitempty"`
+		Message      string `json:"message,omitempty"`
+		Records      []struct {
+			RecordType          string `json:"type,omitempty"`
+			Hostname            string `json:"hostname,omitempty"`
+			DestinationHostname string `json:"destinationHostname,omitempty"`
+			Ip                  string `json:"ip,omitempty"`
+			Address             string `json:"address,omitempty"`
+			Text                string `json:"text,omitempty"`
+		} `json:"data,omitempty"`
+		Command string `json:"command,omitempty"`
+	}
+
+	r, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer r.Body.Close()
+
+	json.NewDecoder(r.Body).Decode(&response)
+
+	var records []libdns.Record
+
+	for _, record := range response.Records {
+		var value string
+		if record.DestinationHostname != "" {
+			value = record.DestinationHostname
+		} else if record.Ip != "" {
+			value = record.Ip
+		} else if record.Address != "" {
+			value = record.Address
+		} else if record.Text != "" {
+			value = record.Text
+		}
+
+		records = append(records, libdns.Record{
+			Type:  record.RecordType,
+			Name:  record.Hostname,
+			Value: value,
+		})
+
+	}
+
+	return records, nil
 }
 
 // AppendRecords adds records to the zone. It returns the records that were added.
